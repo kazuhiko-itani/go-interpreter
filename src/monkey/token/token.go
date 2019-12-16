dac53ece3f7c8cd290fa1db8ab34f5a9fd66c075
kazuhiko-itani <kazuhiko.itani@gmail.com>
2019-12-16 10:11:18 +0900

    add EQ NOT_EQ

---
+++ src/monkey/token/token.go
@@ -0,0 +1,66 @@
+package token
+
+// TokenType トークンの型
+type TokenType string
+
+// Token tokenss
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// トークン定義
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"   // 134343
+
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	EQ 		 = "=="
+	NOT_EQ = "!="
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE 		 = "TRUE"
+	FALSE		 = "FALSE"
+	IF			 = "IF"
+	ELSE		 = "ELSE"
+	RETURN	 = "RETURN"
+)
+
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
