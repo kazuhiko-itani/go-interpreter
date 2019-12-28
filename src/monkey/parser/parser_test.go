e5bd87688c57c96db9de3a037ed6c3df6c980400
kazuhiko-itani <kazuhiko.itani@gmail.com>
2019-12-28 10:40:50 +0900

    refactor test

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -172,10 +172,12 @@ func TestParsingPrefixExpressions(t *testing.T) {
 	prefixTests := []struct {
 		input 			 string
 		operator 		 string
-		integerValue int64
+		value interface{}
 	} {
 		{ "!5;", "!", 5 },
 		{ "-15;", "-", 15 },
+		{ "!true;", "!", true },
+		{ "!false;", "!", false },
 	}
 
 	for _, tt := range prefixTests {
@@ -205,7 +207,7 @@ func TestParsingPrefixExpressions(t *testing.T) {
 						tt.operator, exp.Operator)
 		}
 
-		if !testIntegerLiteral(t, exp.Right, tt.integerValue) {
+		if !testLiteralExpression(t, exp.Right, tt.value) {
 			return
 		}
 	}
@@ -214,9 +216,9 @@ func TestParsingPrefixExpressions(t *testing.T) {
 func TestParsingInfixExpressions(t *testing.T) {
 	infixTests := []struct {
 		input 		 string
-		leftValue  int64
+		leftValue  interface{}
 		operator   string
-		rightValue int64
+		rightValue interface{}
 	} {
 		{ "5 + 5;", 5, "+", 5 },
 		{ "5 - 5;", 5, "-", 5 },
@@ -226,6 +228,9 @@ func TestParsingInfixExpressions(t *testing.T) {
 		{ "5 < 5;", 5, "<", 5 },
 		{ "5 == 5;", 5, "==", 5 },
 		{ "5 != 5;", 5, "!=", 5 },
+		{ "true == true", true, "==", true },
+		{ "true != false", true, "!=", false },
+		{ "false == false", false, "==", false },
 	}
 
 	for _, tt := range infixTests {
@@ -245,21 +250,45 @@ func TestParsingInfixExpressions(t *testing.T) {
 					program.Statements[0])
 		}
 
-		exp, ok := stmt.Expression.(*ast.InfixExpression)
+		if !testInfixExpression(t, stmt.Expression, tt.leftValue, tt.operator, tt.rightValue) {
+			return
+		}
+	}
+}
+
+func TestBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input 					string
+		expectedBoolean bool
+	} {
+		{ "true", true },
+		{ "false", false },
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program has not enough statements. got=%d",
+				len(program.Statements))
+		}
+
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
 		if !ok {
-			t.Fatalf("stmt is not ast.PrefixExpression. got=%T", stmt.Expression)
+			t.Fatalf("program.Statement[0] is not ast.ExpressionStatement. got=%T",
+				program.Statements[0])
 		}
 
-		if !testIntegerLiteral(t, exp.Left, tt.leftValue) {
-			return
+		boolean, ok := stmt.Expression.(*ast.Boolean)
+		if !ok {
+			t.Fatalf("exp not *ast.Boolean. got=%T", stmt.Expression)
 		}
-
-		if exp.Operator != tt.operator {
-			t.Fatalf("exp.Operator is not '%s'. got=%s", tt.operator, exp.Operator)
-		}
-
-		if !testIntegerLiteral(t, exp.Right, tt.rightValue) {
-			return
+		if boolean.Value != tt.expectedBoolean {
+			t.Errorf("boolean.Value not %t. got=%t", tt.expectedBoolean,
+				boolean.Value)
 		}
 	}
 }
@@ -304,6 +333,25 @@ func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
 	return true
 }
 
+func testBooleanLiteral(t *testing.T, exp ast.Expression, value bool) bool {
+	bo, ok := exp.(*ast.Boolean)
+	if !ok {
+		t.Errorf("exp not *ast.Boolean. got=%T", exp)
+		return false
+	}
+
+	if bo.Value != value {
+		t.Errorf("bo.Value not %t. got=%t", value, bo.Value)
+		return false
+	}
+	if bo.TokenLiteral() != fmt.Sprintf("%t", value) {
+		t.Errorf("bo.TokenLiteral not %t. got=%s", value, bo.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
 func testLiteralExpression(
 	t *testing.T,
 	exp ast.Expression,
@@ -316,6 +364,8 @@ func testLiteralExpression(
 			return testIntegerLiteral(t, exp, v)
 		case string:
 			return testIdentifier(t, exp, v)
+		case bool:
+			return testBooleanLiteral(t, exp, v)
 	}
 	t.Errorf("type of exp not handled. got=%T", exp)
 	return false
@@ -375,6 +425,22 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 		{
 			"3 + 4 * 5 == 3 * 1 + 4 * 5",
 			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+		{
+			"true",
+			"true",
+		},
+		{
+			"false",
+			"false",
+		},
+		{
+			"3 > 5 == false",
+			"((3 > 5) == false)",
+		},
+		{
+			"3 < 5 == true",
+			"((3 < 5) == true)",
 		},
 	}
 
