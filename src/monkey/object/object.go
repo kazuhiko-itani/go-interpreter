34ee5bfab24095e311a8161b28ae0019946272f5
kazuhiko-itani <kazuhiko.itani@gmail.com>
2019-12-29 12:01:46 +0900

    add apply function evaluator

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"fmt"
+	"bytes"
+	"strings"
+	"monkey/ast"
+)
 
 type ObjectType string
 
@@ -8,6 +13,7 @@ const (
 	INTEGER_OBJ 		 = "INTEGER"
 	BOOLEAN_OBJ 		 = "BOOLEAN"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	FUNCTION_OBJ 		 = "FUNCTIOn"
 	ERROR_OBJ 			 = "ERROR"
 	NULL_OBJ 				 = "NULl"
 )
@@ -37,6 +43,31 @@ type ReturnValue struct {
 
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body 			 *ast.BlockStatement
+	Env				 *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
 
 type Error struct {
 	Message string
