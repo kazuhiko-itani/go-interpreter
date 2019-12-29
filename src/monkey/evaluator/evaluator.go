e9c42cad48080fa1943f6e6cf5677c3ae242c79f
kazuhiko-itani <kazuhiko.itani@gmail.com>
2019-12-29 10:15:25 +0900

    add return evaluator

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -14,7 +14,7 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 		case *ast.Program:
-			return evalStatements(node.Statements)
+			return evalProgram(node)
 		case *ast.ExpressionStatement:
 			return Eval(node.Expression)
 		case *ast.PrefixExpression:
@@ -29,19 +29,26 @@ func Eval(node ast.Node) object.Object {
 		case *ast.Boolean:
 			return nativeBooltoBooleanObject(node.Value)
 		case *ast.BlockStatement:
-			return evalStatements(node.Statements)
+			return evalBlockStatement(node)
 		case *ast.IfExpression:
 			return evalIfExpression(node)
+		case *ast.ReturnStatement:
+			val := Eval(node.ReturnValue)
+			return &object.ReturnValue{Value: val}
 	}
 
 	return nil
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range stmts {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
 	}
 
 	return result
@@ -131,6 +138,20 @@ func nativeBooltoBooleanObject(input bool) *object.Boolean {
 	}
 
 	return FALSE
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+
+	return result
 }
 
 func evalIfExpression(ie *ast.IfExpression) object.Object {
--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ 		= "NULl"
+	INTEGER_OBJ 		 = "INTEGER"
+	BOOLEAN_OBJ 		 = "BOOLEAN"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	NULL_OBJ 				 = "NULl"
 )
 
 type Object interface {
@@ -29,7 +30,16 @@ type Boolean struct {
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
 
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
 type Null struct {}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string { return "null" }
+
+
